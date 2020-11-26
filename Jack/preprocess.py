2aeb8c526ca2cb0cc0872abaeac4faab245f0471
Jack Procaccini <jproc1222@gmail.com>
2020-11-25 21:22:50 -0500

    Fixed bug in preprocessor causing labels to be extracted improperly

--- Jack/preprocess.py
+++ Jack/preprocess.py
@@ -32,7 +32,7 @@ def save_mfcc(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512, nu
         if dirpath is not dataset_path:
             
             # save the semantic label
-            dirpath_components = dirpath.split("/") # genre/blues => [genre, blues]
+            dirpath_components = dirpath.split("\\") # genre/blues => [genre, blues]
             semantic_label = dirpath_components[-1]
             data["mapping"].append(semantic_label)
             print("\nProcessing {}".format(semantic_label))
